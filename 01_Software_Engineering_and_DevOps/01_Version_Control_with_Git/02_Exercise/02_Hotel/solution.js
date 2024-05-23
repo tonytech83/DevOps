aee2471c7db8379a13ef455ce679196f5e7dd883
tonytech83 <a.petrov.83@gmail.com>
2024-05-23 13:30:15 +0300

    Implemented Our Offers form functionality

--- 01_Software_Engineering_and_DevOps/01_Version_Control_with_Git/02_Exercise/02_Hotel/solution.js
+++ 01_Software_Engineering_and_DevOps/01_Version_Control_with_Git/02_Exercise/02_Hotel/solution.js
@@ -1,42 +1,39 @@
-changeContent('search-form-content');
-document.querySelector('#search-form-button').addEventListener('click', (e) => searchFormData(e));
+document.querySelector('#search-back-btn').addEventListener('click', (e) => fillSearchForm(e));
 
-function searchFormData(e) {
+function fillSearchForm(e) {
     e.preventDefault();
-    const data = e.target.parentElement;
-    const checkIn = data.querySelector('#check-in').value;
-    const checkOut = data.querySelector('#check-out').value;
-    const people = data.querySelector('#people').value;
-    if (checkIn != '' && checkOut != '' && people != '' &&
-        new Date(checkIn) <= new Date(checkOut)) {
-        reservation.startDate = checkIn;
-        reservation.endDate = checkOut;
-        reservation.guestsCount = people;
-        console.log(reservation);
-        changeContent('search-result-form-content');
-    }
-}
-
-let reservation =
-{
-    startDate: null,
-    endDate: null,
-    guestsCount: 0,
-    roomType: null,
-    name: null,
-    phone: null,
-    email: null
-}
-
-function changeContent(className) {
-    document.querySelectorAll('.custom-form').forEach(div => div.classList.add('hidden'));
-    if( document.querySelector(`.${className}`) != null){
-    document.querySelector(`.${className}`).classList.remove('hidden');
-    }
-}
-
-document.querySelector('#new-reservation').addEventListener('click', (e) => cleanData(e));
-
-function cleanData(e) {
     changeContent('search-form-content');
+    document.querySelector('#check-in').value = reservation.startDate;
+    document.querySelector('#check-out').value = reservation.endDate;
+    document.querySelector('#people').value = reservation.guestsCount;
 }
+
+
+document.querySelectorAll('.room-type').forEach(room => {
+    room.addEventListener("click", (e) => selectRoomType(e))
+});
+
+function selectRoomType(e) {
+    let myTarget = undefined;
+    e.preventDefault;
+    if (e.target.querySelector('img') != null) {
+        myTarget = e.target;
+    } else {
+        myTarget = e.target.parentElement;
+    }
+    document.querySelectorAll('.room-type').forEach(room =>
+        room.classList.remove('selected-room'));
+    myTarget.classList.add('selected-room');
+}
+
+document.querySelector('#search-next-btn').addEventListener('click', (e) => findRoom(e));
+
+function findRoom(e) {
+    e.preventDefault();
+    const roomInfo = e.target.parentElement.parentElement.querySelector('.selected-room h4').textContent;
+    reservation.roomType = roomInfo;
+    console.log(reservation);
+    changeContent('guest-details-form-content');
+
+}
+
